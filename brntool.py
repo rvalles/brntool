d9e0e257a68bd9e6e98e1c5c6138155f491acbe8
Jan-Philipp Litza <janphilipp@litza.de>
2012-06-23 00:43:26 +0200

    fixed hex-conversion for 32 bit

--- brntool.py
+++ brntool.py
@@ -23,7 +23,7 @@ def memreadblock(ser,addr,size):
 	ser.write('r')
 	while not (ser.read(1)=='0' and ser.read(1)=='x'):
 		pass
-	ser.write(hex(addr)[2:])
+	ser.write(hex(addr)[2:-1 if addr.__class__ == long else len(hex(addr))])
 	ser.write('\r')
 	while not (ser.read(1)=='.' and ser.read(1)=='.' and ser.read(1)=='.'):
 		pass
