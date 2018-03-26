07dbce822e5c69412e28504e638c4e9f23bf4767
Steffen Rehberg <steffen.rehberg@googlemail.com>
2018-03-26 20:02:30 +0200

    Fix TypeError when using Python 3
    (line 45 'str' object cannot be interpreted as an integer)

--- brntool.py
+++ brntool.py
@@ -42,7 +42,7 @@ def memreadblock(ser,addr,size):
 		m = lineregex.match(ser.readline().decode().strip())
 	while m:
 		if sys.version_info >= (3, 0):
-			buf+=bytes(m.group(1)[1:].split(' '))
+			buf+=bytes.fromhex(m.group(1))
 		else:
 			buf+=''.join([chr(int(x, 16)) for x in m.group(1)[1:].split(' ')])
 		m = lineregex.match(ser.readline().decode().strip())
